071e2b68d43943f50b669124b9d1ea6290d282a1
loic <lgm.loic@gmail.com>
2022-10-01 19:01:41 +0200

    update form + services.yaml

--- src/Form/ArtworkType.php
+++ src/Form/ArtworkType.php
@@ -6,6 +6,7 @@ use App\Entity\Artwork;
 use App\Entity\Category;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -21,6 +22,12 @@ class ArtworkType extends AbstractType
             ->add('category', EntityType::class, [
                 'class' => Category::class,
                 'choice_label' => 'name'
+            ])
+            ->add('images', FileType::class, [
+                'label' => false,
+                'multiple' => true,
+                'mapped' => false,
+                'required' => false
             ])
         ;
     }
